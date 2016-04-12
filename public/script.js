9e859e3346de34d28211928d7a48c04f02e97f43
berwin <berwin7996@gmail.com>
2016-04-12 00:00:27 -0500

    all four graphs, kinda ugly

--- public/script.js
+++ public/script.js
@@ -1,15 +1,26 @@
 // read in the files into a list or something or another
 // 'songscrape/jsonfiles/countrysongs.txt.json.sentiment.json',
 var songs = [];
+var money_songs = [];
+var family_songs = [];
+var drug_songs = [];
+var relationships_songs = [];
+
 $.getJSON( './jsonfiles/countrysongs.txt.json.sentiment.json', function( data ) {
   songs = songs.concat(data);
-  console.log(data);
+  $.getJSON( './jsonfiles/rapsongs.txt.json.sentiment.json', function( data ) {
+    songs = songs.concat(data);
+    for (var i = 0; i < songs.length; i++) {
+      if (songs[i].hasOwnProperty('category')){
+        if (songs[i]['category'] === 'money') money_songs.push(songs[i]);
+        if (songs[i]['category'] === 'family') family_songs.push(songs[i]);
+        if (songs[i]['category'] === 'drugs') drug_songs.push(songs[i]);
+        if (songs[i]['category'] === 'relationships') relationships_songs.push(songs[i]);
+      }
+    }
+    showgraphs();
+  });
 });
-$.getJSON( './jsonfiles/rapsongs.txt.json.sentiment.json', function( data ) {
-  songs = songs.concat(data);
-  console.log(data);
-});
-
 
 
 var margin = {top: 20, right: 30, bottom: 40, left: 30},
@@ -36,28 +47,29 @@ var tip = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-10, 0])
   .html(function(d) {
-    return "<strong>Value:</strong> <span style='color:red'>" + d.value + "</span>";
+    return "<strong>Value:</strong> <span style='color:red'>" + d.song + ", " + d.sentiment + "</span>";
   })
+var showgraphs = function () {
+  // MONEY
+  var svg = d3.select("#money").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  svg.call(tip);
 
-svg.call(tip);
-
-d3.csv("data.csv", type, function(error, data) {
-  x.domain(d3.extent(data, function(d) { return d.value; })).nice();
-  y.domain(data.map(function(d) { return d.name; }));
+  x.domain(d3.extent(money_songs, function(d) { return d.sentiment; })).nice();
+  y.domain(money_songs.map(function(d) { return d.song; }));
 
   svg.selectAll(".bar")
-      .data(data)
+      .data(money_songs)
     .enter().append("rect")
       .attr("class", function(d) { return "bar bar--" + (d.genre == 'country' ? "country" : "rap"); })
-      .attr("x", function(d) { return x(Math.min(0, d.value)); })
-      .attr("y", function(d) { return y(d.name); })
-      .attr("width", function(d) { return Math.abs(x(d.value) - x(0)); })
+      .attr("x", function(d) { return x(Math.min(0, d.sentiment)); })
+      // .attr("y", function(d) { return y(d.name); })
+      .attr("y", function(d) { return y(d.song); })
+      .attr("width", function(d) { return Math.abs(x(d.sentiment) - x(0)); })
       .attr("height", y.rangeBand())
       .on('mouseover', tip.show)
       .on('mouseout', tip.hide);
@@ -66,9 +78,94 @@ d3.csv("data.csv", type, function(error, data) {
       .attr("class", "y axis")
       .attr("transform", "translate(" + x(0) + ",0)")
       .call(yAxis);
-});
 
-function type(d) {
-  d.value = +d.value;
-  return d;
+
+  // drugs
+  var svg = d3.select("#drugs").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.call(tip);
+
+  x.domain(d3.extent(drug_songs, function(d) { return d.sentiment; })).nice();
+  y.domain(drug_songs.map(function(d) { return d.song; }));
+
+  svg.selectAll(".bar")
+      .data(drug_songs)
+    .enter().append("rect")
+      .attr("class", function(d) { return "bar bar--" + (d.genre == 'country' ? "country" : "rap"); })
+      .attr("x", function(d) { return x(Math.min(0, d.sentiment)); })
+      // .attr("y", function(d) { return y(d.name); })
+      .attr("y", function(d) { return y(d.song); })
+      .attr("width", function(d) { return Math.abs(x(d.sentiment) - x(0)); })
+      .attr("height", y.rangeBand())
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + x(0) + ",0)")
+      .call(yAxis);
+
+
+  // family
+  var svg = d3.select("#family").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.call(tip);
+
+  x.domain(d3.extent(family_songs, function(d) { return d.sentiment; })).nice();
+  y.domain(family_songs.map(function(d) { return d.song; }));
+
+  svg.selectAll(".bar")
+      .data(family_songs)
+    .enter().append("rect")
+      .attr("class", function(d) { return "bar bar--" + (d.genre == 'country' ? "country" : "rap"); })
+      .attr("x", function(d) { return x(Math.min(0, d.sentiment)); })
+      // .attr("y", function(d) { return y(d.name); })
+      .attr("y", function(d) { return y(d.song); })
+      .attr("width", function(d) { return Math.abs(x(d.sentiment) - x(0)); })
+      .attr("height", y.rangeBand())
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + x(0) + ",0)")
+      .call(yAxis);
+
+
+  // relationships
+  var svg = d3.select("#relationships").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.call(tip);
+
+  x.domain(d3.extent(relationships_songs, function(d) { return d.sentiment; })).nice();
+  y.domain(relationships_songs.map(function(d) { return d.song; }));
+
+  svg.selectAll(".bar")
+      .data(relationships_songs)
+    .enter().append("rect")
+      .attr("class", function(d) { return "bar bar--" + (d.genre == 'country' ? "country" : "rap"); })
+      .attr("x", function(d) { return x(Math.min(0, d.sentiment)); })
+      // .attr("y", function(d) { return y(d.name); })
+      .attr("y", function(d) { return y(d.song); })
+      .attr("width", function(d) { return Math.abs(x(d.sentiment) - x(0)); })
+      .attr("height", y.rangeBand())
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + x(0) + ",0)")
+      .call(yAxis);
 }
